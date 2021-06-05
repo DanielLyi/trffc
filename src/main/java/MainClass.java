73426ce629f174f26eca58ebfebf32eebeac17e0
Danylo <danylo.liakh@gmail.com>
2021-06-05 10:22:03 +0300

    Added example code

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,5 +1,8 @@
+import com.core.Algorithms;
+
 public class MainClass {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
+        System.out.println(Algorithms.getFastestRoute());
     }
 }
